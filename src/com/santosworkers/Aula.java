5b3ac7a97c3591afb4cd5c68df817b5b4f00c2ad
gabrieldocs <mydocs.lucas@gmail.com>
2021-04-09 07:43:29 -0300

    adicionando streams de entrada e saída em console e utilizando o JPtionPane

---
+++ src/com/santosworkers/Aula.java
@@ -0,0 +1,9 @@
+package com.santosworkers;
+
+import javax.swing.JOptionPane;
+
+public class Aula {
+    public static void main(String [] args) {
+        JOptionPane.showMessageDialog(null, "Bem vindo ao grupo de estudos de Java/Oracle");
+    }
+}
